b74f007198f575f2ba42aa6037bdef963bb7a14d
elizabeth-reeves <libby_reeves@sbcglobal.net>
2020-09-19 22:34:15 -0500

    added writing to csv, adjusted flags

--- facemask_tracking.py
+++ facemask_tracking.py
@@ -10,15 +10,20 @@ import argparse
 import cv2
 import os
 from utils import backbone
+import datetime
+import collections
+import pandas as  pd
 
 
 def cumulative_facemask_counting(input_video, detection_graph, category_index,
                                  is_color_recognition_enabled, roi, deviation,
-                                 targeted_objects=None):
+                                 targeted_objects=None, output=None, include_zeros=False,
+                                 live_view=True, write_video_out=False):
     """
     Count objects passing across the ROI; keep a cumulative count.
 
     """
+    dct = collections.defaultdict(list)
 
     # input video
     cap = cv2.VideoCapture(input_video)
@@ -26,16 +31,17 @@ def cumulative_facemask_counting(input_video, detection_graph, category_index,
 
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    # new_width = min(600, width)
-    # new_height = int(new_width / width) * height
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    output_movie = cv2.VideoWriter('./the_output.avi', fourcc, fps, (width, height))
 
     roi_position = int(roi * width)
 
     total_unmasked = 0
     total_masked = 0
     total_unknown = 0
+    total_all = 0
     with detection_graph.as_default():
         with tf.compat.v1.Session(graph=detection_graph) as sess:
 
@@ -65,6 +71,9 @@ def cumulative_facemask_counting(input_video, detection_graph, category_index,
             # for all the frames that are extracted from input video
 
             while True:
+                this_masked = 0
+                this_unmasked = 0
+                this_unknown = 0
                 ret, frame = cap.read()
 
                 if not ret:
@@ -103,6 +112,8 @@ def cumulative_facemask_counting(input_video, detection_graph, category_index,
 
 
                 label = ''
+                # datetime, frame_id, #masked_Frame, #unmasked_frame, #unknown_frame,
+                # cumulative_masked, #cumulative_unmaksed, #cumulative unknown, #cumulativeTotal
                 for (box, pred) in zip(locs, preds):
                     # unpack the bounding box and predictions
                     (startX, startY, endX, endY) = box
@@ -120,8 +131,10 @@ def cumulative_facemask_counting(input_video, detection_graph, category_index,
                     cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
                     if "No" in label:
                         total_unmasked += counter
+                        this_unmasked = counter
                     else:
                         total_masked += counter
+                        this_masked = counter
 
                 # when the vehicle passed over line and counted, make the color of ROI line green
                 if counter == 1:
@@ -133,6 +146,19 @@ def cumulative_facemask_counting(input_video, detection_graph, category_index,
 
                 if label == '':
                     total_unknown += counter
+                    this_unknown = counter
+
+                this_total = this_masked + this_unmasked + this_unknown
+                total_all += this_total
+
+                # datetime, frame_id, #masked_Frame, #unmasked_frame, #unknown_frame,
+                # cumulative_masked, #cumulative_unmaksed, #cumulative unknown, #cumulativeTotal
+
+                lst_for_dict = [this_masked, this_unmasked, this_unknown, this_total,
+                                total_masked, total_unmasked, total_unknown, total_all]
+
+                if this_total > 0 or include_zeros:
+                    dct[datetime.datetime.now(tz=None)] = lst_for_dict
 
                 # insert information text to video frame
                 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -177,12 +203,22 @@ def cumulative_facemask_counting(input_video, detection_graph, category_index,
                     2,
                     cv2.LINE_AA,
                 )
-
-                cv2.imshow('object counting', input_frame)
+                if live_view:
+                    cv2.imshow('object counting', input_frame)
+                if write_video_out:
+                    output_movie.write(input_frame)
 
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
 
+            if output != None and output != "None":
+                df = pd.DataFrame.from_dict(dct).T
+            # datetime, frame_id, #masked_Frame, #unmasked_frame, #unknown_frame,
+            # cumulative_masked, #cumulative_unmaksed, #cumulative unknown, #cumulativeTotal
+                df.columns = ["masked_frame", "unmasked_frame", "unknown_frame",
+                          "total_frame", "cumulative_masked", "cumulative_unmasked",
+                          "cumulative_unknown", "cumulative_total"]
+                df.to_csv(output)
             cap.release()
             cv2.destroyAllWindows()
 
@@ -201,6 +237,16 @@ def run():
     ap.add_argument("-d", "--deviation",
                     default=10,
                     help="margin from ROI for detection")
+    ap.add_argument("-c", "--csv_out",
+                    default="./output.csv",
+                    help="file path where output csv will be written")
+    ap.add_argument("-v", "--video_out",
+                    default=False,
+                    help="file path where output annotated video will be written")
+    ap.add_argument("-l", "--live_view",
+                    default=True,
+                    help="whether the annotated frames will be displayed as they are produced"
+                    )
     args = vars(ap.parse_args())
 
     # By default I use an "SSD with Mobilenet" model here. See the detection model zoo (
@@ -212,15 +258,16 @@ def run():
 
     is_color_recognition_enabled = 0
 
+    input_source = args["input"]
     if args["type"] == "v":
         # handle the case where the input is a camera id for streaming
-        if args["input"].isdecimal():
-            input_source = int(args["input"])
-        else:
-            input_source = args["input"]
+        if type(args["input"]) == str:
+            if args["input"].isdecimal():
+                input_source = int(args["input"])
         cumulative_facemask_counting(input_source, detection_graph, category_index,
                                      is_color_recognition_enabled, args["roi"], args["deviation"],
-                                     targeted_objects="person")
+                                     targeted_objects="person", output=args["csv_out"],
+                                     live_view=args["live_view"], write_video_out=args["video_out"])
     else:
         # the input is an image type (must be specified; video is assumed)
         detect_mask_image.mask_image_filename(args["input"])
--- utils/backbone.py
+++ utils/backbone.py
@@ -6,7 +6,7 @@ def set_model(model_name, label_name):
     model_found = 0
 
     for file in glob.glob("*"):
-        if (file == model_name):
+        if file == model_name:
             model_found = 1
 
     # What model to download.
@@ -14,7 +14,7 @@ def set_model(model_name, label_name):
     model_file = model_name + '.tar.gz'
     download_base = 'http://download.tensorflow.org/models/object_detection/'
 
-    # Path to frozen detection graph. This is the actual model that is used for the object detection.
+    # Path to frozen detection graph. The actual model that is used for the object detection.
     path_to_ckpt = model_name + '/frozen_inference_graph.pb'
 
     # List of the strings that is used to add correct label for each box.
@@ -23,7 +23,7 @@ def set_model(model_name, label_name):
     num_classes = 90
 
     # Download Model if it has not been downloaded yet
-    if (model_found == 0):
+    if model_found == 0:
         opener = urllib.request.URLopener()
         opener.retrieve(download_base + model_file, model_file)
         tar_file = tarfile.open(model_file)
@@ -41,8 +41,10 @@ def set_model(model_name, label_name):
             od_graph_def.ParseFromString(serialized_graph)
             tf.import_graph_def(od_graph_def, name='')
 
-# Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts 5, we know that this corresponds to airplane. Here I 		use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+# Loading label map Label maps map indices to category names, so that when our convolution network
+    # predicts 5, we know that this corresponds to airplane. Here I 		use internal utility
+    # functions, but anything that returns a dictionary mapping integers to appropriate string
+    # labels would be fine
     label_map = label_map_util.load_labelmap(path_to_labels)
     categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=num_classes, use_display_name=True)
     category_index = label_map_util.create_category_index(categories)
